793735a7f082c1e6be5555fc6baf643a6a4590e7
Mayukoooo <yamagishi.work3@gmail.com>
2023-12-29 19:36:45 +0900

    resolved: isPalindrome

---
+++ src/isPalindrome/index.ts
@@ -0,0 +1,14 @@
+type IsPalindrome = (x: number) => boolean;
+
+const isPalindrome: IsPalindrome = (x) => {
+  if (x < 0) return false;
+
+  const xToString = String(x);
+  for (let i = 0; i < xToString.length; i++) {
+    if (xToString[i] !== xToString[xToString.length - (1 + i)]) {
+      return false;
+    }
+  }
+  return true;
+};
+console.log(isPalindrome(120));
