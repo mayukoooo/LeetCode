9270cbb2b81d67bc5e6f522e9c7b158286d6dc1d
Mayukoooo <yamagishi.work3@gmail.com>
2024-01-12 13:44:11 +0900

    add: line

--- src/memo/euclideanAlgorithm/index.ts
+++ src/memo/euclideanAlgorithm/index.ts
@@ -6,6 +6,7 @@ const euclideanAlgorithm: EuclideanAlgorithm = (m, n) => {
   if (n === 0) return m;
   return euclideanAlgorithm(n, m % n);
 };
+
 const main = (() => {
   console.log(euclideanAlgorithm(51, 15)); // 3
   console.log(euclideanAlgorithm(15, 51)); // 3
