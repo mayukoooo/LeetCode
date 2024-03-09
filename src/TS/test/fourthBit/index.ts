e2f2a31f87697f2769988ab1a73d8c14b5542b3f
Mayukoooo <yamagishi.work3@gmail.com>
2024-03-10 00:24:18 +0900

    chore: .gitignore

--- src/TS/test/fourthBit/index.ts
+++
@@ -1,9 +0,0 @@
-type FourthBit = (number: number) => number;
-const fourthBit: FourthBit = (number) => {
-  const binaryNumber = number.toString(2);
-  const last = Array.from(binaryNumber).pop();
-  return Number(last);
-};
-console.log(fourthBit(77));
-console.log(fourthBit(32));
-console.log(fourthBit(0));
