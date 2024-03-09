ae0599cf5cadb7f7cbc38fd7ffe35f4f3cf88d7f
Mayukoooo <yamagishi.work3@gmail.com>
2024-03-10 00:18:50 +0900

    chore: fix directory structure

--- src/TS/easy/plusOne/best/index.ts
+++ src/TS/easy/plusOne/best/index.ts
@@ -1,5 +1,5 @@
 // 問題文に不備あり
-import { PlusOne } from "..";
+import { PlusOne } from "../index";
 
 const plusOne: PlusOne = (digits) => {
   return (BigInt(digits.join("")) + BigInt(1)).toString().split("").map(Number);
